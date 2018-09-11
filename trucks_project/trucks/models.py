0a946fdfd93851d739a17c710a3ec621991f36fa
Pobeguts Ivan <Ivan.Pobeguts@aton.ru>
2018-09-11 13:40:37 +0300

    add percentage to output table

--- trucks_project/trucks/models.py
+++ trucks_project/trucks/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Truck(models.Model):
-    number = models.CharField(max_length=20)
-    truck_model = models.CharField(max_length=20)
-    max_weight = models.IntegerField(default=0)
-    current_weight = models.IntegerField(default=0)
+    number = models.CharField(max_length=20, verbose_name='number')
+    truck_model = models.CharField(max_length=20, verbose_name='truck_model')
+    max_weight = models.IntegerField(default=0, verbose_name='max_weight')
+    current_weight = models.IntegerField(default=0, verbose_name='current_weight')
 
--- trucks_project/trucks/views.py
+++ trucks_project/trucks/views.py
@@ -3,15 +3,20 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from django.db.models import IntegerField, Case, Value, When, F, Func
+
 from .models import Truck
 
 
-import logging
-
-logger = logging.getLogger(__name__)
-
-
 def index(request):
-    trucks_list = Truck.objects.all()
+    trucks_list = Truck.objects.all().annotate(
+        percentage=Case(
+            When(max_weight__lt=F('current_weight'),
+                 then=Func(100 * (F('max_weight') - F('current_weight')) / F('max_weight'), function='ABS')),
+            default=Value(0),
+            output_field=IntegerField(),
+        ),
+    )
+
     context = {'trucks_list': trucks_list}
     return render(request, 'truck/index.html', context)
